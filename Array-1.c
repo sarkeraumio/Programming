38f9158ddd50c67a86be46abcb4f7f746caa72cb
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2022-03-12 12:36:20 +0600

    Add files via upload

---
+++ Array-1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i;
+
+    printf("Enter the number of elements you want to take input: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(i=1;i<=n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for(i=1;i<=n;i++){
+        printf("%d %d\n",i,arr[i]);
+    }
+
+
+    return 0;
+}
